9c72f4210f33c41a180145b304bd760bbf4a53bc
Marcel Birkner <marcel.birkner@codecentric.de>
2014-06-01 12:16:25 +0200

    MB: added curl smoketests for REST API

---
+++ smoketests/smoketest.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+# 
+# Smoketests for REST API
+
+# define variables
+SERVER=http://localhost:9000
+
+# helper methods
+validate() {
+    echo "Response HTTP Code: $1"
+    if [ $1 != $2 ]; then
+      echo "Expected $2";
+	  echo "Exit after error";
+      exit 1;
+    fi
+	echo "";
+}
+
+#######################################################
+# Basic Smoketests
+
+echo "Init MongoDB"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/version`
+validate $httpcode "200"
+
+echo "Init MongoDB"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/init`
+validate $httpcode "200"
+
+
+#######################################################
+# Customer Smoketests
+
+echo "GET all customers"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/customers`
+validate $httpcode "200"
+
+
+#######################################################
+# Project Smoketests
+
+echo "GET all projects"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/projects`
+validate $httpcode "200"
+
+
+#######################################################
+# Employee Smoketests
+
+echo "GET all employees"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/employees`
+validate $httpcode "200"
+
+echo "GET employee by id, that is too short, expect 500"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/employees/101010`
+validate $httpcode "500"
+
+echo "GET employee by id, that does not exist, expect 404"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/employees/1089dee9edcd86a419e8d9f1`
+validate $httpcode "404"
+
+echo "Search employee by email"
+httpcode=`curl -i -o timmiller.tmp -w "%{http_code}" -X GET "$SERVER/api/mongo/search/employee?email=Tim.Miller@company.com" 2>/dev/null`
+validate $httpcode "200"
+id=`cat timmiller.tmp | grep "_id" | awk '{print $2}' | sed 's/"\|,//g'`
+
+echo "Get employee with id $id"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/employees/$id`
+validate $httpcode "200"
+
+#########################################################
+# Add new employee, search employee by name=smoketest to find id, delete employee by id
+ 
+echo "POST new employee"
+httpcode=`curl -H "Content-Type: application/json" -w "%{http_code}" -X POST $SERVER/api/mongo/employees -d @testEmployee.json 2>/dev/null`
+validate $httpcode "200"
+
+echo "Search employee by name=smoketest"
+httpcode=`curl -i -o smoketest.tmp -w "%{http_code}" -X GET "$SERVER/api/mongo/search/employee?name=smoketest" 2>/dev/null`
+validate $httpcode "200"
+id=`cat smoketest.tmp | grep "_id" | awk '{print $2}' | sed 's/"\|,//g'`
+
+echo "Get employee with id $id"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X GET $SERVER/api/mongo/employees/$id`
+validate $httpcode "200"
+
+echo "Delete employee with id $id"
+httpcode=`curl -s -o /dev/null -w "%{http_code}" -X DELETE $SERVER/api/mongo/employees/$id`
+validate $httpcode "200"
+
+#########################################################
+# Update employee, create employee, search employee by name=smoketest to find id, update employee with new data
+
+echo "POST new employee"
+httpcode=`curl -H "Content-Type: application/json" -w "%{http_code}" -X POST $SERVER/api/mongo/employees -d @testEmployee.json 2>/dev/null`
+validate $httpcode "200"
+
+echo "Search employee by name=smoketest"
+httpcode=`curl -i -o smoketest.tmp -w "%{http_code}" -X GET "$SERVER/api/mongo/search/employee?name=smoketest" 2>/dev/null`
+validate $httpcode "200"
+id=`cat smoketest.tmp | grep "_id" | awk '{print $2}' | sed 's/"\|,//g'`
+
+echo "Update existing employee with id $id"
+httpcode=`curl -H "Content-Type: application/json" -w "%{http_code}" -X POST $SERVER/api/mongo/employees/$id -d @testUpdateEmployee.json 2>/dev/null`
+validate $httpcode "200"
+
+
+#########################################################
+
+echo "Clean temporary test data"
+rm -rf *.tmp
+
+echo "Done"
