51defc277f49abcc7723528706def1a9814e0a4e
Marcel Birkner <marcel.birkner@gmail.com>
2014-07-01 15:19:22 +0200

    MB: cleanup directives

--- client/js/directive/employee-directives.js
+++ client/js/directive/employee-directives.js
@@ -4,57 +4,57 @@
   var app = angular.module('employee-directives', []);
 
   app.directive('navigation', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'navigation.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'navigation.html'
+  };
   });
   app.directive('employeeTable', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'employee-table.html'
-	};
-  });
-  app.directive('customerTable', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'customer-table.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'employee-table.html'
+  };
   });
   app.directive('employeeList', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'employee-list.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'employee-list.html'
+  };
   });
   app.directive('employeeForm', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'employee-form.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'employee-form.html'
+  };
   });
   app.directive('skillForm', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'skill-form.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'skill-form.html'
+  };
   });
   app.directive('projectForm', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'project-form.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'project-form.html'
+  };
   });
   app.directive('homeaddressForm', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'homeaddress-form.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'homeaddress-form.html'
+  };
   });
   app.directive('searchaddressForm', function(){
-	return {
-		restrict: 'E',
-		templateUrl: 'searchaddress-form.html'
-	};
+  return {
+    restrict: 'E',
+    templateUrl: 'searchaddress-form.html'
+  };
+  });
+  app.directive('customerTable', function() {
+  return {
+    restrict : 'E',
+    templateUrl : 'customer-table.html'
+  };
   });
 })();
