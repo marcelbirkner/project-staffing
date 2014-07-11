82633ae3994b7aa3324c63c99aab0e5158f2c947
Marcel Birkner <marcel.birkner@codecentric.de>
2014-07-11 14:27:36 +0200

    MB: init deployment script for EC2 server

--- infrastructure/deploySoftware.sh
+++ infrastructure/deploySoftware.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-ls -la 
-echo "Hello World"
+echo "Killing running Node process"
+nodepid=`ps aux | grep node | grep server.js | awk '{print $2}'`
+kill -9 $nodepid
 
+echo "Cleanup existing project"
+rm -rf project-staffing
+
+echo "Get latest source code"
+git clone https://github.com/marcelbirkner/project-staffing.git
+cd project-staffing
+npm install
+cd static
+
+echo "Update IP"
+find . -type f -print0 | xargs -0 sed -i 's/localhost:9000/54.216.107.20:9000/g'
+cd ..
+
+echo "Start NodeJS Process"
+node server.js
 
 
  
