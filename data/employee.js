466d319b0a4d52be76014f1edd97b6d397d2eca8
Marcel Birkner <marcel.birkner@codecentric.de>
2014-07-12 12:23:55 +0200

    MB: added additional test employees

--- data/employee.js
+++ data/employee.js
@@ -1,20 +1,22 @@
-'use strict';
+﻿'use strict';
 
-module.exports = {
+module.exports = [
+  {
 
-   1:{
-    "name": "Max Mustermann",
-    "email": "Max.Mustermann@company.com",
-    "twitter": "@maxm",
-    "address": "Halmstra�e, K�ln, Germany",
-    "office": "Berlin",
+    "name": "Hans",
+    "email": "Hans@company.de",
+    "office": "Karlsruhe",
     "skills": [
       "java",
-      "html",
-      "jenkins",
-      "neo4j",
+      "tomcat",
+      "weld",
+      "seam",
+      "hadoop",
       "big data",
-      "scrum"
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
     ],
     "projects": [
       {
@@ -25,28 +27,387 @@ module.exports = {
       },
       {
         "name": "Smoketest Project 4",
-        "description": "Develop a Java frontend PHP application",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "createdOn": 1405159510965,
+    "address": "Bismarckstraße, Karlsruhe, Germany",
+    "homeaddress": {
+      "longitude": 49.0130359,
+      "latitude": 8.394217099999992
+    }
+  },
+  {
+
+    "name": "Andreas",
+    "email": "andreas@company.de",
+    "office": "Frankfurt",
+    "skills": [
+      "java",
+      "tomcat",
+      "weld",
+      "seam",
+      "hadoop",
+      "big data",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Textorstraße, Frankfurt, Germany",
+    "homeaddress": {
+      "longitude": 50.1005726,
+      "latitude": 8.683852300000012
+    },
+    "createdOn": 1405159475901
+  },
+  {
+
+    "name": "Bastian",
+    "email": "bastian@company.de",
+    "office": "Berlin",
+    "skills": [
+      "java",
+      "tomcat",
+      "weld",
+      "seam",
+      "hadoop",
+      "big data",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Karl-Marx-Straße, Berlin, Germany",
+    "homeaddress": {
+      "longitude": 52.4758572,
+      "latitude": 13.439465499999983
+    },
+    "createdOn": 1405159499022
+  },
+  {
+
+    "name": "Georg",
+    "email": "georg@company.com",
+    "office": "Hamburg",
+    "skills": [
+      "java",
+      "tomcat",
+      "weld",
+      "seam",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Bergedorf, Hamburg, Germany",
+    "homeaddress": {
+      "longitude": 53.48598399999999,
+      "latitude": 10.216255100000012
+    },
+    "createdOn": 1405159526289
+  },
+  {
+
+    "name": "Ingrid",
+    "email": "ingrid@company.com",
+    "office": "München",
+    "skills": [
+      "java",
+      
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Ismaninger Straße, München, Germany",
+    "homeaddress": {
+      "longitude": 48.1431932,
+      "latitude": 11.60132759999999
+    },
+    "createdOn": 1405159535259
+  },
+  {
+
+    "name": "Johannes",
+    "email": "johannes@company.com",
+    "office": "Solingen",
+    "skills": [
+      "java",
+      
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+        "address": "Zietenstraße, Solingen, Germany",
+    "homeaddress": {
+      "longitude": 51.1650905,
+      "latitude": 7.0985961999999745
+    },
+    "createdOn": 1405159547691
+  },
+  {
+
+    "name": "Karl",
+    "email": "karl@company.com",
+    "office": "Düsseldorf",
+    "skills": [
+      
+      "big data",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Königsallee, Dusseldorf, Germany",
+    "homeaddress": {
+      "longitude": 51.2263902,
+      "latitude": 6.7787548000000015
+    },
+    "createdOn": 1405159558332
+  },
+  {
+
+    "name": "Leo",
+    "email": "leo@company.com",
+    "office": "Düsseldorf",
+    "skills": [
+      "java",
+      "tomcat",
+      "weld",
+     
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Erkrath, Dusseldorf, Germany",
+    "homeaddress": {
+      "longitude": 51.2277411,
+      "latitude": 6.773455600000034
+    },
+    "createdOn": 1405159568808
+  },
+  {
+
+    "name": "Martin",
+    "email": "martin@company.com",
+    "office": "Solingen",
+    "skills": [
+      "java",
+      
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    
+    "address": "Landhaus Wupperhof, Solingen, Germany",
+    "homeaddress": {
+      "longitude": 51.137342,
+      "latitude": 7.083703000000014
+    },
+    "createdOn": 1405159581213
+  },
+  {
+
+    "name": "Bastian",
+    "email": "bastian@company.de",
+    "office": "Frankfurt",
+    "skills": [
+      
+      "hadoop",
+      "big data",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
+        "start": "2013-05-01",
+        "end": "2014-09-01"
+      }
+    ],
+    "address": "Hanauer Landstraße, Frankfurt, Germany",
+    "homeaddress": {
+      "longitude": 50.1225871,
+      "latitude": 8.740226699999994
+    },
+    "createdOn": 1405159608310
+  },
+  {
+
+    "name": "Tim",
+    "email": "Tim@company.com",
+    "twitter": "@tim",
+    "address": "Merscheider Strasse, Solingen, Germany",
+    "office": "Solingen",
+    "skills": [
+      "java",
+    
+      "big data",
+      "app dynamics",
+      "spring",
+      "jboss",
+      "jenkins"
+    ],
+    "projects": [
+      {
+        "name": "Smoketest Project 3",
+        "description": "Develop a Java backend Java application",
+        "start": "2012-01-01",
+        "end": "2013-04-01"
+      },
+      {
+        "name": "Smoketest Project 4",
+        "description": "Develop a JavaScript frontend application",
         "start": "2013-05-01",
         "end": "2014-09-01"
       }
     ],
     "homeaddress": {
-      "longitude": 50.9569327,
-      "latitude": 6.913445300000035
+      "longitude": 51.1658323,
+      "latitude": 7.024760499999957
     },
-    "createdOn": 1403896788443
+    "createdOn": 1403896809069
   },
-  2:{
-    "name": "Mark Smith",
-    "email": "Mark.Smith@company.com",
-    "twitter": "@marksmith",
+  {
+
+    "name": "Mark",
+    "email": "Mark@company.com",
+    "twitter": "@marks",
     "address": "Bonn Hbf, Bonn, Germany",
     "office": "Solingen",
     "skills": [
       "java",
-      "html",
-      "javascript",
-      "css"
+      "tomcat",
+      "weld",
+      "big data",
+     
+      "jenkins"
     ],
     "projects": [
       {
@@ -68,18 +429,20 @@ module.exports = {
     },
     "createdOn": 1403896798968
   },
-  3:{
-    "name": "Tim Miller",
-    "email": "Tim.Miller@company.com",
-    "twitter": "@tmiller",
-    "address": "Merscheider Stra�e, Solingen, Germany",
-    "office": "Hamburg",
+  {
+
+    "name": "Max",
+    "email": "Max@company.com",
+    "twitter": "@max",
+    "address": "Halmstraß, Köln, Germany",
+    "office": "Düsseldorf",
     "skills": [
       "java",
-      "tomcat",
-      "weld",
-      "seam",
-      "hadoop"
+      "html",
+      "jenkins",
+      "neo4j",
+      "big data",
+      "scrum"
     ],
     "projects": [
       {
@@ -90,15 +453,15 @@ module.exports = {
       },
       {
         "name": "Smoketest Project 4",
-        "description": "Develop a Java frontend PHP application",
+        "description": "Develop a Java frontend application",
         "start": "2013-05-01",
         "end": "2014-09-01"
       }
     ],
     "homeaddress": {
-      "longitude": 51.1658323,
-      "latitude": 7.024760499999957
+      "longitude": 50.9569327,
+      "latitude": 6.913445300000035
     },
-    "createdOn": 1403896809069
+    "createdOn": 1403896788443
   }
-};
+];
