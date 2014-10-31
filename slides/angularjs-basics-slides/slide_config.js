e9ff80cfd961885fa720f633bc9f2993ee1995a7
Marcel Birkner <marcel.birkner@codecentric.de>
2014-10-31 15:30:35 +0100

    Added github.io webpage to project incl. initial presentation slides

---
+++ slides/angularjs-basics-slides/slide_config.js
@@ -0,0 +1,31 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Agile JavaScript Workshop',
+    subtitle: 'AngularJS Basics',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    favIcon: '../favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  presenters: [{
+    name: 'Ben Ripkens',
+    company: 'IT Consultant<br>codecentric AG',
+    gplus: 'http://plus.google.com/107194541655176085264',
+    twitter: '@benripkens',
+    www: 'http://bripkens.de',
+    github: 'http://github.com/bripkens'
+  }, {
+    name: 'Michael Lex',
+    company: 'IT Consultant<br>codecentric AG',
+    gplus: 'http://plus.google.com/113418074379656254601',
+    twitter: '@mlex',
+    github: 'http://github.com/mlex'
+  }]
+};
---
+++ slides/application-intro-slides/slide_config.js
@@ -0,0 +1,32 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Project-Staffing WebApp',
+    subtitle: 'Introduction',
+    useBuilds: true,
+    usePrettify: true,
+    enableSlideAreas: true,
+    enableTouch: true,
+    favIcon: '../../favicon.ico',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+
+  presenters: [{
+    name: 'Marcel Birkner',
+    company: 'Software Consultant<br>codecentric AG',
+    gplus: 'https://plus.google.com/115899999568491796836',
+    twitter: '@marcelbirkner',
+    www: 'https://blog.codecentric.de/en/author/marcel.birkner/',
+    github: 'http://github.com/marcelbirkner'
+  }, {
+    name: 'Bastian Krol',
+    company: 'Software Consultant<br>codecentric AG',
+    gplus: 'https://plus.google.com/116252915765949444123',
+    twitter: '@bastiankrol',
+    github: 'http://github.com/basti1302'
+  }]
+};
+
