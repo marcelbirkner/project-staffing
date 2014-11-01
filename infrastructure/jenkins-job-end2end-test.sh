6ee6f48baefc86837e945b2af6d9f6169eca5755
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-01 22:11:32 +0100

    Update jenkins-job-end2end-test.sh

--- infrastructure/jenkins-job-end2end-test.sh
+++ infrastructure/jenkins-job-end2end-test.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
 TEST_SERVER=$1
-echo "Running End2End Test $TEST_SERVER"
-
-npm install
 export CHROME_BIN=/usr/bin/chromium-browser
+
+echo "Running End2End Test $TEST_SERVER"
+echo "Update IP"
+find e2e/conf.js -type f -print0 | xargs -0 sed -i "s/localhost:9000/$TEST_SERVER:9000/g"
 
 echo "Kill Selenium Process"
 PID=`ps aux | grep node | grep webdriver | awk '{print $2}'`
 sudo kill -9 $PID 
 
 echo "Install npm packages and reporters"
+npm install
 npm install jasmine-reporters
 
 echo "Start WebDriver Manager"
 webdriver-manager start &
-
-echo "Update IP"
-find e2e/conf.js -type f -print0 | xargs -0 sed -i "s/localhost:9000/$TEST_SERVER:9000/g"
 
 echo "Start E2E test"
 protractor e2e/conf.js
