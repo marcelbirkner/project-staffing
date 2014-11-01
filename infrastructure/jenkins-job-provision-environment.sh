1ee31cb8f2daa5181dd338d8269401fbcb4b59b8
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-01 20:47:40 +0100

    Update and rename jenkins-job-provision-test.sh to jenkins-job-provision-environment.sh

---
+++ infrastructure/jenkins-job-provision-environment.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+ARTIFACT_VERSION=$1
+NEXUS_SERVER=$2
+ENVIRONMENT=$3
+
+echo "Downloading project-staffing-$ARTIFACT_VERSION.zip"
+wget $NEXUS_SERVER/nexus/service/local/repositories/releases/content/de/codecentric/js/project-staffing/$ARTIFACT_VERSION/project-staffing-$ARTIFACT_VERSION.zip 2> wget.log
+
+echo "Stopping NodeJS App"
+KEYPAIR=/var/lib/jenkins/.ssh/eu-west-keypair-1.pem
+ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$ENVIRONMENT 'sudo service project-staffing status'
+ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$ENVIRONMENT "sudo bash -s" < infrastructure/killNodeProcess.sh
+
+echo "Deploying NodeJS App"
+ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$ENVIRONMENT 'sudo rm -rf project-staffing*'
+scp -i $KEYPAIR -o StrictHostKeyChecking=no project-staffing-$ARTIFACT_VERSION.zip ubuntu@$ENVIRONMENT:
+
+echo "Unpacking NodeJS App"
+ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$ENVIRONMENT 'sudo unzip project-staffing-*.zip -d project-staffing > unzip.log'
