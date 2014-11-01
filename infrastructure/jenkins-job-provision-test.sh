21ef0affade2954591376c8005cb90d7cc87ff84
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-01 20:42:33 +0100

    Update jenkins-job-provision-test.sh

--- infrastructure/jenkins-job-provision-test.sh
+++ infrastructure/jenkins-job-provision-test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 ARTIFACT_VERSION=$1
-SERVER=$2
+NEXUS_SERVER=$2
 TEST_SERVER=$3
 
 echo "Downloading project-staffing-$ARTIFACT_VERSION.zip"
-wget $SERVER/nexus/service/local/repositories/releases/content/de/codecentric/js/project-staffing/$ARTIFACT_VERSION/project-staffing-$ARTIFACT_VERSION.zip 2> wget.log
+wget $NEXUS_SERVER/nexus/service/local/repositories/releases/content/de/codecentric/js/project-staffing/$ARTIFACT_VERSION/project-staffing-$ARTIFACT_VERSION.zip 2> wget.log
 
 echo "Stopping NodeJS App"
 KEYPAIR=/var/lib/jenkins/.ssh/eu-west-keypair-1.pem
@@ -18,6 +18,3 @@ scp -i $KEYPAIR -o StrictHostKeyChecking=no project-staffing-$ARTIFACT_VERSION.z
 
 echo "Unpacking NodeJS App"
 ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$TEST_SERVER 'sudo unzip project-staffing-*.zip -d project-staffing > unzip.log'
-
-# echo "Starting NodeJS App"
-# ssh -i $KEYPAIR -o StrictHostKeyChecking=no ubuntu@$TEST_SERVER 'sudo service project-staffing start'
