cfedb7230449f0fefde7f2f009eb56722a02e27c
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-01 20:40:56 +0100

    Update jenkins-job-ci.sh

--- infrastructure/jenkins-job-ci.sh
+++ infrastructure/jenkins-job-ci.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-SERVER=$1
+NEXUS_SERVER=$1
 NEXUS_CREDENTIALS=$2
 BUILD_NUMBER=$3
 
@@ -17,4 +17,4 @@ echo "Package JS App"
 zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/
 
 echo "Upload JS App to Artifact Server"
-curl -v -u $NEXUS_CREDENTIALS --upload-file project-staffing.zip $SERVER/nexus/content/repositories/releases/de/codecentric/js/project-staffing/$BUILD_NUMBER/project-staffing-$BUILD_NUMBER.zip
+curl -v -u $NEXUS_CREDENTIALS --upload-file project-staffing.zip $NEXUS_SERVER/nexus/content/repositories/releases/de/codecentric/js/project-staffing/$BUILD_NUMBER/project-staffing-$BUILD_NUMBER.zip
