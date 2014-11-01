7ad347f1145237852b6eb1fb3423214b26727556
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-01 19:47:33 +0100

    Create ci-job.sh

---
+++ infrastructure/ci-job.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+SERVER = $1
+NEXUS_CREDENTIALS = $2
+BUILD_NUMBER = $3
+
+echo "Build static client assets and run gulp"
+cd client
+npm install
+gulp
+
+echo "Install server side dependencies"
+cd ..
+npm install
+
+echo "Package JS App"
+zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/
+
+echo "Upload JS App to Artifact Server"
+curl -v -u $NEXUS_CREDENTIALS --upload-file project-staffing.zip $SERVER/nexus/content/repositories/releases/de/codecentric/js/project-staffing/$BUILD_NUMBER/project-staffing-$BUILD_NUMBER.zip
