09c3d5b770e2f69cce3e1ddfc104523e231e7588
Marcel Birkner <marcel.birkner@codecentric.de>
2014-11-02 13:13:30 +0100

    Make deployment artifact smaller, removed unnessessary npm packages

--- infrastructure/jenkins-job-ci.sh
+++ infrastructure/jenkins-job-ci.sh
@@ -13,8 +13,15 @@ grunt
 echo "Store Build Version in Navigation"
 find static/navigation.html -type f -print0 | xargs -0 sed -i "s/Company Projects/Version $BUILD_NUMBER/g"
 
-echo "Package JS App"
-zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/
+echo "Package JS App; remove unneccessary packages beforehand"
+rm -rf node_modules/grunt*
+rm -rf node_modules/karma*
+rm -rf node_modules/mocha*
+rm -rf node_modules/sinon*
+rm -rf node_modules/chai* 
+rm -rf node_modules/jasmine-reporters*
+
+zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/ 2> zip.log
 
 echo "Upload JS App to Artifact Server"
 curl -v -u $NEXUS_CREDENTIALS --upload-file project-staffing.zip $NEXUS_SERVER/nexus/content/repositories/releases/de/codecentric/js/project-staffing/$BUILD_NUMBER/project-staffing-$BUILD_NUMBER.zip
