6e0b0b2c841233ab91c8097f144b7941d9526ee8
Marcel Birkner <marcel.birkner@gmail.com>
2014-11-03 23:15:19 +0100

    Update jenkins-job-ci.sh

--- infrastructure/jenkins-job-ci.sh
+++ infrastructure/jenkins-job-ci.sh
@@ -20,8 +20,10 @@ rm -rf node_modules/mocha*
 rm -rf node_modules/sinon*
 rm -rf node_modules/chai* 
 rm -rf node_modules/jasmine-reporters*
+rm -rf node_modules/jshint-jenkins-checkstyle-reporter*
+rm -rf node_modules/.bin*
 
-zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/ 2> zip.log
+zip -r project-staffing.zip server.js static/ node_modules/ data/ lib/
 
 echo "Upload JS App to Artifact Server"
 curl -v -u $NEXUS_CREDENTIALS --upload-file project-staffing.zip $NEXUS_SERVER/nexus/content/repositories/releases/de/codecentric/js/project-staffing/$BUILD_NUMBER/project-staffing-$BUILD_NUMBER.zip
