bfdf838e026bafcdc97be82339df3fc348501a6f
Marcel Birkner <marcel.birkner@codecentric.de>
2014-11-08 13:13:28 +0100

    Updated slide format & styling

--- slides/introduction/slide_config.js
+++ slides/introduction/slide_config.js
@@ -16,14 +16,14 @@ var SLIDE_CONFIG = {
 
   presenters: [{
     name: 'Marcel Birkner',
-    company: 'Software Consultant<br>codecentric AG',
+    company: 'Software Consultant',
     gplus: 'https://plus.google.com/115899999568491796836',
     twitter: '@marcelbirkner',
     www: 'https://blog.codecentric.de/en/author/marcel.birkner/',
     github: 'http://github.com/marcelbirkner'
   }, {
     name: 'Bastian Krol',
-    company: 'Software Consultant<br>codecentric AG',
+    company: 'Software Consultant',
     twitter: '@bastiankrol',
     www: 'https://blog.codecentric.de/en/author/bastian.krol/',
     github: 'http://github.com/basti1302'
