8ff0e86ce9725e395b47e46c5981aaca44b6cc06
Marcel Birkner <marcel.birkner@gmail.com>
2015-06-12 14:39:59 +0200

    Fix checkstyle warning

--- client/js/controller/customer_controller.js
+++ client/js/controller/customer_controller.js
@@ -4,170 +4,187 @@
   'use strict';
 
   angular
-  .module('project-staffing')
-  .controller('CustomerController', function($http, $scope, Url){
+    .module('project-staffing')
+    .controller('CustomerController', function($http, $scope, Url) {
 
-    var url = Url.getUrl();
+      var url = Url.getUrl();
 
-    var company = this;
-    company.customers = [];
-    var drawCircle = false;
-    this.customer = {};
+      var company = this;
+      company.customers = [];
+      var drawCircle = false;
+      this.customer = {};
 
-    $scope.details = {};
-    $scope.options = {};
+      $scope.details = {};
+      $scope.options = {};
 
-    $scope.form = {
-      type: 'geocode',
-      watchEnter: true
-    };
+      $scope.form = {
+        type: 'geocode',
+        watchEnter: true
+      };
 
-    $http.get(url + '/api/mongo/customers').success(function(data) {
+      $http.get(url + '/api/mongo/customers').success(function(data) {
         company.customers = data;
-    });
+      });
 
-    this.addCustomer = function() {
+      this.addCustomer = function() {
         this.customer.companyaddress = {};
         var keys = Object.keys($scope.details.geometry.location);
-        this.customer.companyaddress.longitude = $scope.details.geometry.location[keys[0]];
-        this.customer.companyaddress.latitude = $scope.details.geometry.location[keys[1]];
+        this.customer.companyaddress.longitude = $scope.details.geometry.location[
+          keys[0]];
+        this.customer.companyaddress.latitude = $scope.details.geometry.location[
+          keys[1]];
 
         // TODO: get userid from session
         var user = 'jon';
         var msg = 'created a new customer';
-        var activity = {timestamp: new Date(), subject: user, action: msg, object: this.customer.company};
+        var activity = {
+          timestamp: new Date(),
+          subject: user,
+          action: msg,
+          object: this.customer.company
+        };
         $http.post(url + '/api/mongo/customers', JSON.stringify(this.customer));
         $http.post(url + '/api/mongo/activities', JSON.stringify(activity));
 
         $http.get(url + '/api/mongo/customers').success(function(data) {
-            company.customers = data;
+          company.customers = data;
         });
-    };
+      };
 
-    this.deleteCustomer = function(id) {
+      this.deleteCustomer = function(id) {
 
-      var deletedCustomer;
-      for (var i in company.customers){
-        if( company.customers[i]._id === id ) {
-          deletedCustomer = company.customers[i];
-          company.customers.splice(i,1);
+        var deletedCustomer;
+        for (var i in company.customers) {
+          if (company.customers[i]._id === id) {
+            deletedCustomer = company.customers[i];
+            company.customers.splice(i, 1);
+          }
         }
-      }
 
-      // delete customer in backend
-      $http.delete(url + '/api/mongo/customers/' + id);
+        // delete customer in backend
+        $http.delete(url + '/api/mongo/customers/' + id);
 
-      // TODO: get userid from session
-      var user = 'julia';
-      var msg = 'deleted customer';
-      var activity = {timestamp: new Date(), subject: user, action: msg, object: deletedCustomer.company};
-      $http.post(url + '/api/mongo/activities', JSON.stringify(activity));
+        // TODO: get userid from session
+        var user = 'julia';
+        var msg = 'deleted customer';
+        var activity = {
+          timestamp: new Date(),
+          subject: user,
+          action: msg,
+          object: deletedCustomer.company
+        };
+        $http.post(url + '/api/mongo/activities', JSON.stringify(activity));
 
-    };
+      };
 
-    this.editCustomer = function(id) {
-      for (var i in company.customers){
-        if( company.customers[i]._id === id ) {
-          this.customer = company.customers[i];
+      this.editCustomer = function(id) {
+        for (var i in company.customers) {
+          if (company.customers[i]._id === id) {
+            this.customer = company.customers[i];
+          }
         }
-      }
-    };
+      };
 
-    this.resetForm = function() {
+      this.resetForm = function() {
         this.customer = {};
-    };
+      };
 
-    /** Google Map **/
-    var location = {lat: 51.161295, lng: 7.010175000000004}; // default location
-    var image = {
+      /** Google Map **/
+      var location = {
+        lat: 51.161295,
+        lng: 7.010175000000004
+      }; // default location
+      var image = {
         url: 'img/company-icon.png',
         size: new google.maps.Size(64, 64),
-        origin: new google.maps.Point(0,0),  // The origin for this image is 0,0
+        origin: new google.maps.Point(0, 0), // The origin for this image is 0,0
         anchor: new google.maps.Point(0, 64) // The anchor for this image is the base of the flagpole at 0,64
-    };
+      };
 
-    $scope.details = {};
+      $scope.details = {};
 
-    var customers = [];
-    drawCircle = false;
+      var customers = [];
+      drawCircle = false;
 
-    this.searchCustomers = function() {
+      this.searchCustomers = function() {
 
-      if( $scope.details.geometry ) {
-        location = $scope.details.geometry.location;
-        drawCircle = true;
-      }
+        if ($scope.details.geometry) {
+          location = $scope.details.geometry.location;
+          drawCircle = true;
+        }
 
-      var searchUrl = url + '/api/mongo/customers';
-      $http.get(searchUrl).success(function(data) {
-        customers = data;
-        initializeMap();
-      });
-    };
+        var searchUrl = url + '/api/mongo/customers';
+        $http.get(searchUrl).success(function(data) {
+          customers = data;
+          initializeMap();
+        });
+      };
 
-    /**
-     * Function to initialize Google Map
-     */
-    function initializeMap() {
+      /**
+       * Function to initialize Google Map
+       */
+      function initializeMap() {
 
-      // Create the map
-      var mapOptions = {
+        // Create the map
+        var mapOptions = {
           zoom: 9,
           center: location,
           mapTypeId: google.maps.MapTypeId.ROADMAP
-      };
+        };
 
-      var map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+          mapOptions);
 
-      if ( drawCircle ) {
+        if (drawCircle) {
           var circleOptions = {
-              strokeColor: '#4CC417',
-              strokeOpacity: 0.8,
-              strokeWeight: 2,
-              fillColor: '#4CC417',
-              fillOpacity: 0.35,
-              map: map,
-              center: location,
-              radius: 50000
+            strokeColor: '#4CC417',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#4CC417',
+            fillOpacity: 0.35,
+            map: map,
+            center: location,
+            radius: 50000
           };
 
           // Add the circle for this city to the map.
           new google.maps.Circle(circleOptions);
           new google.maps.Marker({
-              position: location,
-              map: map,
-              title: 'Found Address',
+            position: location,
+            map: map,
+            title: 'Found Address',
           });
-      }
+        }
 
-      // Display multiple markers on a map
-      var infoWindow = new google.maps.InfoWindow();
-      var markerCustomer, i;
-      var customer;
+        // Display multiple markers on a map
+        var infoWindow = new google.maps.InfoWindow();
+        var markerCustomer, i;
+        var customer;
 
-      for (i = 0; i < customers.length; i++) {
+        for (i = 0; i < customers.length; i++) {
           customer = customers[i];
 
           markerCustomer = new google.maps.Marker({
-              position:
-                new google.maps.LatLng(customer.companyaddress.longitude,
-                customer.companyaddress.latitude),
-              map: map,
-              icon: image,
-              title: customer.company,
+            position: new google.maps.LatLng(customer.companyaddress.longitude,
+              customer.companyaddress.latitude),
+            map: map,
+            icon: image,
+            title: customer.company,
           });
 
           // Allow each marker to have an info window
-          google.maps.event.addListener(markerCustomer, 'click', (function(markerCustomer, i) {
+          google.maps.event.addListener(markerCustomer, 'click', (function(
+            markerCustomer, i) {
             return function() {
               var cust = customers[i];
               var content = '<p><b id="customerTitle">' +
                 cust.company + '</b></p><table class="table"><tr>' +
                 '<tr><td><span class="label label-primary">' +
-                'Industry</span></td><td>' + cust.industry + '</td></tr>' +
+                'Industry</span></td><td>' + cust.industry +
+                '</td></tr>' +
                 '<tr><td><span class="label label-primary">Address' +
-                '</span></td><td>' + cust.address + '</td></tr></table>';
+                '</span></td><td>' + cust.address +
+                '</td></tr></table>';
               infoWindow.setContent(content);
               infoWindow.open(map, markerCustomer);
             };
@@ -175,7 +192,7 @@
         }
       }
 
-    google.maps.event.addDomListener(window, 'load', initializeMap);
-  });
+      google.maps.event.addDomListener(window, 'load', initializeMap);
+    });
 
 })();
